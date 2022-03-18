d4f87092bee0dc5ff8d2b9eec6e58859f832f7e2
Jibril <jazimjibril@gmail.com>
2022-03-19 00:18:03 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <quranTem> </quranTem>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import quranTem from "./components/quranTem.vue"
 
-export default {
-  name: 'App',
+ export default {
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+   quranTem,
+  },
+ }
 </script>
 
 <style>
-#app {
+ #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+ }
 </style>
---
+++ src/components/quranTem.vue
@@ -0,0 +1,91 @@
+<template>
+ <div id="temp">
+  <p id="verse">
+   {{ Verse }}
+  </p>
+  <p id="link">
+   <a :href="link" target="_blank">{{ Chapter }}</a>
+  </p>
+  <n-button @click="getVerse" id="shuffle">
+   <i class="fa-solid fa-shuffle"></i
+  ></n-button>
+ </div>
+</template>
+
+<script>
+ import { NButton } from "naive-ui"
+ //  import { NAnchorLink } from "naive-ui"
+ export default {
+  data() {
+   return {
+    Verse: "",
+    Chapter: "",
+    link: "",
+   }
+  },
+  components: {
+   NButton,
+  },
+  methods: {
+   getVerse() {
+    fetch("https://api.muslim-tab.com/verses/english/random")
+     .then((resp) => resp.json())
+     .then((data) => {
+      this.Verse = data.verse
+      this.Chapter = data.reference
+      this.link = data.link
+     })
+   },
+  },
+  created() {
+   this.getVerse()
+  },
+ }
+</script>
+
+<style scoped>
+ template {
+  border: 1px green solid;
+ }
+ #temp {
+  margin: 0 auto;
+  /* border: 1px red dotted; */
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+ }
+ #verse {
+  margin: 0 auto;
+  width: 80%;
+  font-family: "Merienda", cursive;
+  font-weight: 700;
+  font-size: 4em;
+  text-align: center;
+ }
+ #link {
+  margin-top: 5%;
+ }
+ #link a {
+  text-decoration: none;
+  border: 1px gray solid;
+  border-radius: 10px;
+  padding: 9px 20px;
+  font-family: "Pangolin", cursive;
+  color: #000;
+  font-size: 1.1em;
+ }
+ #link a:hover {
+  color: rgb(29, 204, 137);
+  border-color: rgb(29, 204, 137);
+ }
+ #shuffle {
+  font-size: 2em;
+  padding: 0.9em;
+  justify-content: center;
+  margin-top: 5%;
+  border-radius: 10%;
+ }
+</style>
